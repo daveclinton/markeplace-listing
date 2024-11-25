da41de13d2a7ec65f169adcf5c0f9a571165ee4b
daveclinton <clintondavid46@gmail.com>
2024-11-25 13:45:22 +0300

    feat: add marketplcae enums

---
+++ src/common/enums/marketplace-enum.ts
@@ -0,0 +1,13 @@
+export enum MarketplaceType {
+  EBAY = 'EBAY',
+  FACEBOOK = 'FACEBOOK',
+}
+
+export enum ListingStatus {
+  DRAFT = 'DRAFT',
+  PENDING = 'PENDING',
+  ACTIVE = 'ACTIVE',
+  PAUSED = 'PAUSED',
+  ENDED = 'ENDED',
+  FAILED = 'FAILED',
+}
